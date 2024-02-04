fe4af317dddce757425517fc4118b93449a606c1
GGGeorgiev20 <GGGeorgiev20@codingburgas.bg>
2024-02-04 16:38:11 +0200

    Update the build system

--- build.py
+++ build.py
@@ -3,107 +3,172 @@ import sys
 import platform
 import subprocess
 
-dirs = {
-    "src": "src",
-    "obj": "obj",
-    "bin": "bin"
-}
+config_file = "config.yaml"
 
-cflags = f'-std=c++20 -I{dirs["src"]}'
-errflags = "-Wall -Wunused-variable -Wextra -Wno-enum-compare -g -ggdb -fdiagnostics-color=always -static-libstdc++"
+def check_imports():
+    try:
+        import yaml
+    except ImportError:
+        print("ERROR: PyYAML not found. Installing...")
+        subprocess.check_call(['pip', 'install', 'PyYAML'])
 
-clear = False
-run_automatically = True
+def load_config():
+    check_imports()
 
-out = "koral"
+    import yaml
 
-compiled = []
+    with open(config_file, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"FATAL: Couldn't read config: {e}")
+            return None
+
+def create_dirs():
+    for key in config['directories']:
+        dir = config['directories'][key]
+
+        if dir == [] or dir == "":
+            continue
+
+        if not key in config['ignore'] and not os.path.exists(dir):
+            os.makedirs(dir)
+
+def get_flag(flag):
+    result = config['flags'][flag]
+    
+    if result != "None":
+        return result
+    else:
+        return ""
 
 def get_flags():
-    global errflags
+    flags = f"{get_flag('error_flags')} "
 
-    flags = [ "clear", "release" ]
-
-    if len(sys.argv) != 3 or not sys.argv[2] in flags:
-        print("INFO: No flags specified, using default flags")
-        errflags += " -O0"
-        return
-
-    global clear
-    clear = True
-
-    print("INFO: Rebuiding all files...")
-
-    if sys.argv[2] == "release":
-        print("INFO: Building in release mode")
-        errflags += " -O3"
-    else:
-        errflags += " -O0"
-
-def get_properties():
-    global out
-    
     if platform.system() == "Windows":
-        out += ".exe"
+        flags += get_flag('windows_lib_flags')
     else:
-        out += ".out"
+        flags += get_flag('unix_lib_flags')
 
-    global koral_file
-
-    if len(sys.argv) >= 2:
-        koral_file = sys.argv[1]
+    if config['build_type'] == "debug":
+        flags += " -O0"
+    elif config['build_type'] == "release":
+        print("INFO: Building in release mode")
+        flags += " -O3"
     else:
-        print('ERROR: No file specified.\nExiting...')
+        print("FATAL: Invalid build type in config. Exiting...")
         exit(1)
+
+    return flags
+
+def get_args():
+    args = {
+        "clear": False,
+        "run": False
+    }
+
+    default_args = True
+
+    for arg in sys.argv[1:]:
+        if arg in args:
+            args[arg] = True
+            default_args = False
+        else:
+            print(f"WARNING: Invalid flag '{arg}'. Ignoring...")
+
+    if default_args:
+        print("BUILD: No flags specified, using default flags")
+
+    return args
+
+def get_output_extension():
+    if platform.system() == "Windows":
+        return "exe"
+    else:
+        return "out"
     
-def create_folders():
-    for key in dirs:
-        if not os.path.exists(dirs[key]):
-            os.makedirs(dirs[key])
+def clear():
+    print("BUILD: Rebuilding all files...\n")
+
+    for file in os.listdir(config['directories']['obj']):
+        file_path = os.path.join(config['directories']['obj'], file)
+        os.remove(file_path)
+
+def run(output_path):
+    print("\nBUILD: Running build...\n")
+
+    command = output_path
+
+    if platform.system() == "Windows":
+        command = output_path.replace('/', '\\')
+    else:
+        command = f"./{output_path}"
+    
+    os.system(command)
 
 def run_command(*args):
     try:
         return subprocess.check_output(" ".join(args), shell=True).strip().decode("utf-8")
-    except:
-        print(f'ERROR: Error when compiling.\nExiting...')
+    except Exception as e:
+        print(f'ERROR: Failed to run command:')
+        print(f'ERROR: {e}')
+        print("ERROR: Exiting...")
         exit(1)
 
 def compile(dir, file):
-    if (file.endswith(".cpp")):
-        compiledFile = os.path.join(dir, file)
-        objectFile = os.path.join(dirs["obj"], file.split('.')[0]) + '.o'
+    if (not file.endswith(".cpp")):
+        return
+    
+    compiled_file = os.path.join(dir, file)
+    object_file = os.path.join(config['directories']['obj'], file.split('.')[0]) + '.o'
 
-        compiled.append(objectFile)
+    global compiled
+    compiled.append(object_file)
 
-        if not clear and os.path.exists(objectFile) and os.path.getmtime(compiledFile) < os.path.getmtime(objectFile):
-            return
-        
-        run_command(f'g++ {cflags} {errflags} -c {compiledFile} -o {objectFile}')
+    if os.path.exists(object_file) and os.path.getmtime(compiled_file) < os.path.getmtime(object_file):
+        return
+    
+    run_command(f"{config['compiler_version']} {flags} -c {compiled_file} -o {object_file}")
 
-        post_index = len(dirs["src"]) + 1
-        print(f'BUILD: Compiled {compiledFile[post_index:]}')
+    post_index = len(config['directories']['src']) + 1
+    print(f'Compiled {compiled_file[post_index:]}')
 
 def main():
-    get_flags()
-    create_folders()
+    global config
+    config = load_config()
 
-    get_properties()
+    if not config:
+        print("ERROR: No config file found. Exiting...")
+        exit(1)
+    
+    create_dirs()
 
-    for [dir, _, files] in os.walk(dirs["src"]):
+    global sys_args
+    sys_args = get_args()
+
+    global flags
+    flags = get_flags()
+
+    global compiled
+    compiled = []
+
+    if sys_args['clear']:
+        clear()
+
+    for [dir, _, files] in os.walk(config['directories']['src']):
         for file in files:
             compile(dir, file)
 
-    run_command(f'g++ {cflags} {errflags} -o {dirs["bin"]}/{out} {" ".join(compiled)}')
+    output_extension = get_output_extension()
+    output_path = f"{config['directories']['bin']}/{config['output']}.{output_extension}"
 
-    print("INFO: Build successful")
+    run_command(f"{config['compiler_version']} -std={config['language_version']} {flags} -o {output_path} {' '.join(compiled)} {get_flag('end_flags')}")
 
-    if run_automatically:
-        print("\nINFO: Running program...")
+    print("\nBUILD: Build successful")
 
-        if platform.system() == "Windows":
-            os.system(f"bin\\{out} {koral_file}")
-        else:
-            os.system(f"./bin/{out} {koral_file}")
+    if sys_args['run']:
+        run(output_path)
 
 if __name__ == "__main__":
     main()
